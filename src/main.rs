f85fdb94fbf2e27ab6e2d1bb0053f4acec15e0ab
t1 <senthilktmz@outlook.com>
2025-02-02 22:05:34 +0530

    add model describe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "mycli", version = "1.0", about = "A CLI tool")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
+
+#[derive(Subcommand)]
+enum Commands {
+    Model {
+        #[command(subcommand)]
+        subcommand: ModelSubCommand,
+    },
+}
+
+#[derive(Subcommand)]
+enum ModelSubCommand {
+    Describe { model_file_path: PathBuf },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Model { subcommand } => match subcommand {
+            ModelSubCommand::Describe { model_file_path } => {
+                println!("{:?}", model_file_path);
+            }
+        },
+    }
+}
+
+//
+//
+//
+//
+//
+//
+//
